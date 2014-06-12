4a117ee024cab54c54f645c82dbfae16fcce7543
Ryan P Hansen <rphansen91@gmail.com>
2014-06-11 19:21:49 -0700

    ngView animate

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,4 +1,4 @@
-angular.module('OWMApp',['ngRoute'])
+angular.module('OWMApp',['ngRoute','ngAnimate'])
 	.value('owmCities', ['New York','Dallas','Chicago'])
 	.config(function ($routeProvider) {
 		$routeProvider.when('/', {
@@ -22,6 +22,19 @@ angular.module('OWMApp',['ngRoute'])
 		}).otherwise({
 			redirectTo : '/error'
 		})
+	})
+	.run(function ($rootScope, $location, $timeout) {
+		$rootScope.$on('$routeChangeError', function () {
+			$location.path('/error');
+		});
+		$rootScope.$on('$routeChangeStart', function () {
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function () {
+			$timeout(function() {
+				$rootScope.isLoading = false;
+			}, 1000);
+		});
 	})
 	.controller('HomeCtrl', function ($scope) {
 		$scope.hello = "Welcome Home";
